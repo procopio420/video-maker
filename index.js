6b0dc83ad66b359505752cb1c3dd45e306a6e139
procopio420 <lucas-procopio@outlook.com>
2020-05-03 00:53:07 -0300

    fixing bugs from user input and creating content data model

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const readline = require('readline-sync');
 
 function start() {
-    const content = {};
+    const content = {
+        searchTerm: '',
+        prefix: '',
+        sourceContentOriginal: '',
+        sourceContentSanitized: '',
+        sentences: [{
+            text: '',
+            keywords: [''],
+            images: ['']
+        }]
+    };
 
     function askAndReturnSearchTerm() {
         return readline.question('Type a Wikipedia search term: ');
@@ -11,7 +21,7 @@ function start() {
 
     function askAndReturnPrefix(searchTerm) {
         const prefixes = [`Who is ${searchTerm}`, `What is ${searchTerm}`, `The history of ${searchTerm}`];
-        return prefixes[readline.keyInSelect(prefixes, 'Choose one option:')];
+        return prefixes[readline.keyInSelect(prefixes, 'Choose one option:')].replace(` ${searchTerm}`, '');
     }
 
 
