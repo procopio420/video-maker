d02e7f36a539cdf6760bf1fe3f55803a0bb7d417
procopio420 <lucas-procopio@outlook.com>
2020-05-03 03:49:31 -0300

    add user option to languages

--- index.js
+++ index.js
@@ -1,31 +1,29 @@
 const readline = require('readline-sync');
+const robots = {
+    text: require('./robots/text.js'),
+}
 
-function start() {
-    const content = {
-        searchTerm: '',
-        prefix: '',
-        sourceContentOriginal: '',
-        sourceContentSanitized: '',
-        sentences: [{
-            text: '',
-            keywords: [''],
-            images: ['']
-        }]
-    };
+async function start() {
+    const content = {};
+
+    function askAndReturnLang(){
+        const languages = ['pt', 'en', 'es', 'fr'];
+        return languages[readline.keyInSelect(languages, 'Choose one language: ')];
+    }
+    content.lang = askAndReturnLang();
 
     function askAndReturnSearchTerm() {
         return readline.question('Type a Wikipedia search term: ');
     }
-
     content.searchTerm = askAndReturnSearchTerm();
 
     function askAndReturnPrefix(searchTerm) {
         const prefixes = [`Who is ${searchTerm}`, `What is ${searchTerm}`, `The history of ${searchTerm}`];
-        return prefixes[readline.keyInSelect(prefixes, 'Choose one option:')].replace(` ${searchTerm}`, '');
+        return prefixes[readline.keyInSelect(prefixes, 'Choose one search prefix:')].replace(` ${searchTerm}`, '');
     }
-
-
     content.prefix = askAndReturnPrefix(content.searchTerm);
+
+    await robots.text(content);
 
     console.log(content);
 }
